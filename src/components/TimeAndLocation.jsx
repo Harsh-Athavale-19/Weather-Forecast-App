9b96455aa01d8a207f212df59beb78fbcc984661
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2023-03-09 22:39:57 +0530

    Completed as Old Model

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
+import { formatToLocalTime } from "../services/weatherService";
 
-const TimeAndLocation = () => {
-    return (
-        <div>
-            <div className='flex items-center justify-center my-6'>
-                <p className='text-white text-xl font-extralight'>
-                    Tuesday, 31 May 2022 | Local time: 12:46 PM
-                </p>
-            </div>
+const TimeAndLocation = ({ weather: { dt, timezone, name, country } }) => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          {formatToLocalTime(dt, timezone)}
+        </p>
+      </div>
 
-            <div className='flex items-center justify-center my-3'>
-                <p className='text-white text-3xl font-medium'>
-                    Berlin, DE
-                </p>
-            </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+          {`${name}, ${country}`}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default TimeAndLocation
+export default TimeAndLocation;
